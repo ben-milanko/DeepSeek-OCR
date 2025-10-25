787aee3871814b09240ad6e9cb3f52054a93ab75
Ben Milanko <ben.milanko@protonmail.com>
2025-10-25 22:11:37 +1100

    Add test image

--- DeepSeek-OCR-master/DeepSeek-OCR-hf/run_dpsk_ocr.py
+++ DeepSeek-OCR-master/DeepSeek-OCR-hf/run_dpsk_ocr.py
@@ -10,17 +10,20 @@ model_name = 'deepseek-ai/DeepSeek-OCR'
 
 
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-model = AutoModel.from_pretrained(model_name, _attn_implementation='flash_attention_2', trust_remote_code=True, use_safetensors=True)
-model = model.eval().cuda().to(torch.float16)
-
-
+model = AutoModel.from_pretrained(
+    model_name, 
+    _attn_implementation='flash_attention_2', 
+    trust_remote_code=True, 
+    use_safetensors=True,
+    torch_dtype=torch.float16,
+    device_map="auto"
+)
+model = model.eval()
 
 # prompt = "<image>\nFree OCR. "
 prompt = "<image>\n<|grounding|>Convert the document to markdown. "
-image_file = 'your_image.jpg'
-output_path = 'your/output/dir'
-
-
+image_file = '/home/ben/repos/DeepSeek-OCR/PXL_20251001_052145054.jpg'
+output_path = '/home/ben/repos/DeepSeek-OCR/output'
 
 # infer(self, tokenizer, prompt='', image_file='', output_path = ' ', base_size = 1024, image_size = 640, crop_mode = True, test_compress = False, save_results = False):
 
@@ -31,4 +34,13 @@ output_path = 'your/output/dir'
 
 # Gundam: base_size = 1024, image_size = 640, crop_mode = True
 
-res = model.infer(tokenizer, prompt=prompt, image_file=image_file, output_path = output_path, base_size = 1024, image_size = 640, crop_mode=True, save_results = True, test_compress = True)
+res = model.infer(tokenizer,
+    prompt=prompt,
+    image_file=image_file,
+    output_path = output_path,
+    base_size = 1024,
+    image_size = 640,
+    crop_mode=True,
+    save_results = True,
+    test_compress = True
+)
